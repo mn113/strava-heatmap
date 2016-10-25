e35c70d13d3cae6cd71ef3519713232e215f99ac
Martin Nicholson <recyclebing+github@gmail.com>
2016-10-25 13:58:15 +0100

    Uncommit config

--- config.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Strava-app-specific data:
-$clientId = 14325;
-$clientSecret = 'e35ef28fc1c6a843c2bda5e185440968de99f1dc';
-$accessToken = '1638809775d55c0e9aecf470e4352d89a19a74a5';
-
-// Google API key:
-$gmaps_key = 'AIzaSyA3nqF6Dq6FDNY9PSFNffwd2IhfY068wuI';
